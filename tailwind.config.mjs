580f77a4b635db8d8f49a3eab295cb85230a3fe0
fabian-martinez-rincon <fabian.martinez181531@alumnos.info.unlp.edu.ar>
2024-03-15 15:23:41 -0300

    Agregando animacion de los videos

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,7 +10,27 @@ export default {
 				'custom-text': '#dcddde',
 				'font-blog':'#0f0f0f'
 			},
+			animation: {
+				"skew-scroll": "skew-scroll 24s linear infinite",
+			},
 		},
+		keyframes: {
+			"skew-scroll": {
+			  "0%": {
+				transform:
+				  "rotatex(13deg) rotateZ(-13deg) skewX(13deg) translateZ(0) translateY(110%)",
+			  },
+			  "95%": {
+				opacity: 1,
+			  },
+			  "100%": {
+				transform:
+				  "rotatex(13deg) rotateZ(-13deg) skewX(13deg) translateZ(0) translateY(-430%)",
+				  opacity: 0,
+			  },
+			},
+	  
+		  },
 	},
 	plugins: [],
 }
