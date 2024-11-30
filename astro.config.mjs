547a1ac7a6b0d005adc3c0bd3177b7e753e0e87a
Fabian-Martinez-Rincon <fabianmartinezrincon.123@gmail.com>
2024-11-30 14:15:13 -0300

    Agregando cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
-
 import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://rincondelfabo.vercel.app/'
+  site: 'https://rincondelfabo.vercel.app/',
+  vite: {
+    ssr: {
+      noExternal: ['@fontsource-variable/onest'], // Agrega aquí el paquete con problemas
+    },
+  },
 })
